07bedf123f0326c05a2462c40fade9b93726b8bb
Alexey <4pok45@mail.ru>
2019-07-03 01:59:40 +0300

    refactore

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,23 +1,36 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class ProductPage(BasePage):
 
+    def take_product_name(self):
+        product = self.driver.find_element(*ProductPageLocators.PRODUCT_NAME)
+        return product.text
+
+    def take_product_price(self):
+        price = self.driver.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        return price.text
+
     def press_button_add_in_basket(self):
         button_add_in_basket = self.driver.find_element(*ProductPageLocators.BUTTON_ADD_IN_BASKET)
         button_add_in_basket.click()
-#        WebDriverWait(self.driver, 3).until(EC.alert_is_present())
-#        self.solve_quiz_and_get_code()
 
-    def should_be_product_name_in_adding_message(self):
+    def should_be_product_name_in_adding_message(self, name):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME)
         product = self.driver.find_element(*ProductPageLocators.PRODUCT_NAME)
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE)
         name_in_message = self.driver.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE)
-        assert product.text in name_in_message.text, f"added {product.text}, but in basket added {name_in_message.text}"
+        assert product.text == name_in_message.text, f"{product.text} does not match {name_in_message.text}"
+        assert name == name_in_message.text, f"added {name} on adding page, but in basket added {name_in_message.text}"
+        assert name == product.text, f"added {name} on adding page, but after name of product was {product.text}"
 
-    def should_be_price_of_product_in_basket_value(self):
-        price = self.driver.find_element(*ProductPageLocators.PRODUCT_PRICE)
+    def should_be_price_of_product_in_basket_value(self, price):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE)
+        product_price = self.driver.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        assert self.is_element_present(*ProductPageLocators.PRICE_IN_MESSAGE)
         price_in_message = self.driver.find_element(*ProductPageLocators.PRICE_IN_MESSAGE)
-        assert price.text in price_in_message.text, f"product price does not match basket price"
+        assert product_price.text == price_in_message.text, "product price does not match basket price"
+        assert price == price_in_message.text, \
+            "product price on adding page does not match basket price when product added"
+        assert price == product_price.text, f"{price} != {product_price}"
--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,29 @@
 from .pages.product_page import ProductPage
+import pytest
 
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_can_add_product_to_cart(driver):
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_cart(driver, link):
     product_page = ProductPage(driver, link)
     product_page.open()
+    product_page.take_product_name()
+    product_page.take_product_price()
     product_page.press_button_add_in_basket()
     product_page.solve_quiz_and_get_code()
-    product_page.should_be_product_name_in_adding_message()
-    product_page.should_be_price_of_product_in_basket_value()
+    product_page.should_be_product_name_in_adding_message(name=product_page.take_product_name())
+    product_page.should_be_price_of_product_in_basket_value(price=product_page.take_product_price())
 
 
 
